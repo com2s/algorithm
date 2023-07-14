0a2fded2337973743979b86092b781ee92fa1f5a
com2s <52689322+com2s@users.noreply.github.com>
2023-07-14 17:08:49 +0900

    Create 1974. 스도쿠 검증.py

---
+++ D2/1974. 스도쿠 검증.py
@@ -0,0 +1,39 @@
+#import sys
+#sys.stdin = open("input.txt", "r")
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    # ///////////////////////////////////////////////////////////////////////////////////
+    S = []
+    result = 1
+    for i in range(9):
+        S.append(list(map(int, input().split())))
+        
+    for i in range(9):
+        if len(set(S[i])) != 9:
+            result = 0
+            
+    for i in range(9):
+        D = []
+        for j in range(9):
+            D.append(S[j][i])
+            
+        if len(set(D)) != 9:
+            result = 0        
+                   
+    K = []
+    for i in range(9):
+        K.append(S[i][0:3])
+        K.append(S[i][3:6])
+        K.append(S[i][6:9])
+          
+    for i in range(3):
+        for j in range(3):
+            U = []
+            U = K[j+9*i] + K[j+3+9*i] + K[j+6+9*i]
+            if len(set(U)) != 9:
+                result = 0
+                
+        
+    print('#{} {}'.format(test_case, result))
