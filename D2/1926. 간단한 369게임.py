f7fae6ab35ccb4f9b46ff55cb86207c7cefb25b0
com2s <52689322+com2s@users.noreply.github.com>
2023-09-01 14:54:41 +0900

    Create 1926. 간단한 369게임.py

---
+++ D2/1926. 간단한 369게임.py
@@ -0,0 +1,15 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PTeo6AHUDFAUq&categoryId=AV5PTeo6AHUDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+N = int(input())
+
+def f(n):
+    n = str(n)
+    c1 = n.count('3')
+    c2 = n.count('6')
+    c3 = n.count('9')
+    if c1 or c2 or c3:
+        return '-'*(c1+c2+c3)
+    else:
+        return n
+    
+for i in range(1,N+1):
+    print(f(i),end=' ')
